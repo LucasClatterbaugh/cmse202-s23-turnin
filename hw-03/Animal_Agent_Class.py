812be0f5376173d895aeeeafdde0a5298688a89e
Lucas Clatterbaugh <Clatter2@msu.edu>
2023-03-31 22:44:33 -0400

    updated for part2

--- hw-03/Animal_Agent_Class.py
+++ hw-03/Animal_Agent_Class.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import itertools
+import random
 
 class Animal_Agent():
 
@@ -170,3 +171,72 @@ class Animal_Agent():
         return child_list
 
 
+
+
+class Mouse(Animal_Agent):
+    def set_allele(self, alle_d, alle_m):
+         
+
+         self.alle_d = alle_d
+         self.alle_m = alle_m
+        
+        # Here we set light animal color to be dominant.
+         if self.alle_d + self.alle_m >= 1:
+             self.color = 'goldenrod'
+             self.catch_probability = 0.95
+            
+         else:
+             self.color = "saddlebrown" 
+             self.catch_probability = 0.05
+    
+
+
+    def procreate(self,all_agents,time):
+        '''
+        Method to create new agents (procreate). There are three barriers to procreation:
+        1. Has it been enough time between the last procreation? (I.e., more than the gestation time.)
+        2. Is there a (male) agent within range?
+        3. Is the population below the saturation point? This is modeled as a logistic function.
+        If all of these conditions are met, then a new litter is created. Every member of the new litter is 
+        randomly assigned genes from their parents. The last litter time is set to the current time and the
+        list of children is returned. 
+
+        all_agents (list): A list of all of the animal agent objects 
+        time (int): The current time, used to determine if the Animal Agent has passed the gestation period.
+        '''
+        
+        child_list = []
+        val = random.random()
+        if (self.sex == 'F') and (np.mod(abs(time-self.last_litter_time),self.gestation) == 0):
+            for agent in all_agents:
+                if (agent.ptype=="Prey") and (agent.sex == "M") and (self.get_distance(agent) <= self.mate_range) and (val > (1.0/(1.0+np.exp(-len(all_agents)/self.saturation_pop)))):
+                    child_num = int(np.random.normal(7,1))
+                    for jj in range(child_num):
+                        mom_a = self.alle_d
+                        if np.random.randint(0,2) == 1:
+                            mom_a = self.alle_m
+                        dad_a = agent.alle_d
+                        if np.random.randint(0,2) == 1:
+                            dad_a = agent.alle_m               
+                        child = Mouse(sex="F")
+                        if np.random.randint(0,2) == 1:
+                            child = Mouse(sex="M")
+                        child.set_allele(dad_a,mom_a)
+                        child_list.append(child)
+                    self.last_litter_time = time
+                    break
+
+        return child_list 
+
+
+
+class Hawk(Animal_Agent):
+        def hunt(self,all_agents):
+            
+            for agent in all_agents:
+                if (agent.ptype == "Prey") and (agent.age < agent.age_limit) and not (agent.is_caught):
+                    if self.get_distance(agent) < self.catch_radius:
+                        val = random.random()
+                        if val > agent.catch_probability:
+                            agent.is_caught = True
+                            return agent   
